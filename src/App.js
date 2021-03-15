160e369688c9871be379844870bc41196515bad5
kavindadilshan <kavindadilshan97@gmail.com>
2021-03-15 14:29:00 +0530

    understanding JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi! React</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>Hi! React</h1>
+  //   </div>
+  // );
+  return  React.createElement('div',null,React.createElement('h1',null,'work'))
 }
 
 export default App;
