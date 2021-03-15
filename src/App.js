f4004f96270e31e8484f015cec31b294cc8b1a91
kavindadilshan <kavindadilshan97@gmail.com>
2021-03-15 15:10:21 +0530

    state handling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
-import React from 'react';
+import React ,{Component}from 'react';
 import Person from './Person/Person';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Hi! React</h1>
-            <Person name={"kavinda"} age={"25"}/>
-            <Person name={"dilshan"} age={"24"}>My Hobbies:racing</Person>
-            <Person name={"nilupulie"} age={"25"}/>
-        </div>
-    );
+class App extends Component{
+    state={
+        personList:[
+            {name:'kavinda',age:'24'},
+            {name:'dilshan',age:'56'},
+            {name:'nilu',age:'24'}
+        ]
+    }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi! React</h1>
+                <Person name={this.state.personList[0].name} age={this.state.personList[0].age}/>
+                <Person name={this.state.personList[1].name} age={this.state.personList[1].age}>My Hobbies:racing</Person>
+                <Person name={this.state.personList[2].name} age={this.state.personList[2].age}/>
+            </div>
+        );
+    }
+
+
     // return  React.createElement('div',null,React.createElement('h1',null,'work'))
 }
 
