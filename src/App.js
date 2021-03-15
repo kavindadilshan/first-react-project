b095b35b12577a7fd5700ef3677db87ebc307f67
kavindadilshan <kavindadilshan97@gmail.com>
2021-03-15 20:33:21 +0530

    add delete function

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component{
         const persons = this.state.personList;
         persons.splice(selectedIndex,1);
         this.setState({personList:persons})
+        console.log(this.state.personList)
     }
 
     render() {
