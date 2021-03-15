14ffca029440771c0903ea0271bd70457a282475
kavindadilshan <kavindadilshan97@gmail.com>
2021-03-15 14:56:50 +0530

    Working with props

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React from 'react';
 import Person from './Person/Person';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi! React</h1>
-        <Person/>
-        <Person/>
-        <Person/>
-    </div>
-  );
-  // return  React.createElement('div',null,React.createElement('h1',null,'work'))
+    return (
+        <div className="App">
+            <h1>Hi! React</h1>
+            <Person name={"kavinda"} age={"25"}/>
+            <Person name={"dilshan"} age={"24"}>My Hobbies:racing</Person>
+            <Person name={"nilupulie"} age={"25"}/>
+        </div>
+    );
+    // return  React.createElement('div',null,React.createElement('h1',null,'work'))
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,17 @@
-const person =()=>{
-    return <p>I'm a person! I,m {Math.floor(Math.random()*30)}</p>
+const person =(props)=>{
+    return <p>I'm a {props.name}! I,m {props.age} years old</p>
 }
 
 export default person;
+
+// import React, {Component} from 'react';
+//
+// class Person extends Component {
+//     render() {
+//         return (
+//             <p>I'm a {this.props.name}! I,m {this.props.age} years old</p>
+//         );
+//     }
+// }
+//
+// export default Person;
