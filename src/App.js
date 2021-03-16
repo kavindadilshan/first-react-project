cf1e31f069a9daa106856503c44cf46134f69a73
kavindadilshan <kavindadilshan97@gmail.com>
2021-03-16 12:07:03 +0530

    add media query using radium,styleroot

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
-import React ,{Component}from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 
-class App extends Component{
-    state={
-        personList:[
-            {id:0,name:'kavinda',age:'24'},
-            {id:1,name:'dilshan',age:'56'},
-            {id:2,name:'nilu',age:'24'}
+class App extends Component {
+    state = {
+        personList: [
+            {id: 0, name: 'kavinda', age: '24'},
+            {id: 1, name: 'dilshan', age: '56'},
+            {id: 2, name: 'nilu', age: '24'}
         ]
     };
 
 
-    onNameChange=(event,id)=>{
-        const personIndex=this.state.personList.findIndex(p=>{
+    onNameChange = (event, id) => {
+        const personIndex = this.state.personList.findIndex(p => {
             return p.id === id
         });
 
@@ -23,69 +23,71 @@ class App extends Component{
         // const person = Object.assign({},this.state.personList[personIndex])  // create object
 
         person.name = event.target.value;
-        const persons =[...this.state.personList];
+        const persons = [...this.state.personList];
         persons[personIndex] = person
 
-        this.setState({personList:persons})
+        this.setState({personList: persons})
     }
 
-    onDeleteHandler=(selectedIndex)=>{
+    onDeleteHandler = (selectedIndex) => {
         const persons = this.state.personList;
-        persons.splice(selectedIndex,1);
-        this.setState({personList:persons})
+        persons.splice(selectedIndex, 1);
+        this.setState({personList: persons})
         console.log(this.state.personList)
     }
 
     render() {
-        const style={
-            backgroundColor:'yellow'
+        const style = {
+            backgroundColor: 'yellow'
         }
-        const btnStyle={
+        const btnStyle = {
             backgroundColor: 'white',
-            borderRadius:10,
-            padding:10,
-            cursor:'pointer',
-            margin:20,
-            ':hover':{
-                backgroundColor:'blue',
-                color:'white'
+            borderRadius: 10,
+            padding: 10,
+            cursor: 'pointer',
+            margin: 20,
+            ':hover': {
+                backgroundColor: 'blue',
+                color: 'white'
             }
         }
 
-        btnStyle.backgroundColor='green'
+        btnStyle.backgroundColor = 'green'
 
-        const list = this.state.personList.map((items,i)=>(
+        const list = this.state.personList.map((items, i) => (
             <Person
                 name={items.name}
                 age={items.age}
                 key={i}
-                onClick={()=>this.onDeleteHandler(i)}
-                onChange={(event)=>this.onNameChange(event,items.id)}
+                onClick={() => this.onDeleteHandler(i)}
+                onChange={(event) => this.onNameChange(event, items.id)}
             />
         ))
 
         // const classes=["Color","Font"].join(" ") //bind css classes
 
-        const classes=[];
+        const classes = [];
 
-        if (this.state.personList.length<=2){
+        if (this.state.personList.length <= 2) {
             classes.push("Color")
         }
 
-        if (this.state.personList.length<=1){
+        if (this.state.personList.length <= 1) {
             classes.push("Font")
         }
 
 
         return (
-            <div className="App">
-                <h1 className={classes.join(" ")}>Hi! React</h1>
-                <button style={btnStyle}>Toggle Button</button>
-                {/*<Person name={this.state.personList[0].name} age={this.state.personList[0].age} onChange={this.onNameChange}/>*/}
-                {/*<Person name={this.state.personList[1].name} age={this.state.personList[1].age}>My Hobbies:racing</Person>*/}
-                {/*<Person name={this.state.personList[2].name} age={this.state.personList[2].age}/>*/}
-                {list}
-            </div>
+            <StyleRoot>
+                <div className="App">
+                    <h1 className={classes.join(" ")}>Hi! React</h1>
+                    <button style={btnStyle}>Toggle Button</button>
+                    {/*<Person name={this.state.personList[0].name} age={this.state.personList[0].age} onChange={this.onNameChange}/>*/}
+                    {/*<Person name={this.state.personList[1].name} age={this.state.personList[1].age}>My Hobbies:racing</Person>*/}
+                    {/*<Person name={this.state.personList[2].name} age={this.state.personList[2].age}/>*/}
+                    {list}
+                </div>
+            </StyleRoot>
         );
     }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,15 @@ import "./Person.css";
 import Radium from 'radium';
 
 const person =(props)=>{
+
+    const style={
+        '@media(min-width:500px)':{
+            width:450
+        }
+    }
+
     return (
-        <div className={"Person"} >
+        <div className={"Person"} style={style}>
             <p onClick={props.onClick}> I'm a {props.name}! I,m {props.age} years old</p>
             <p>{props.children}</p>
             <input type={"text"} onChange={props.onChange}/>
